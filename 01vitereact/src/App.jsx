0d57802a39976dce4bf3e9b3949d61a8e0529321
Romana <romana@Romana.lan>
2025-11-06 23:46:19 +1100

    good start

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Learn from "./Learn"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Learn/>
+  <h1>Test 2</h1>
+  </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Learn.jsx
@@ -0,0 +1,7 @@
+function Learn(){
+    return(
+    <h2>Test component</h2>
+    )
+}
+
+export default Learn
